5eae1b2b3d51a9805d420504bd85af0509cbe89e
Jessy Marion <jessymarionmail@gmail.com>
2023-04-07 12:45:06 +0200

    feat: contact page 70Ã%

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,9 +1,23 @@
 import styles from "./Form.module.scss";
-
+//import Button from "../Button/Button";
 function Form() {
   return (
     <div className={styles.formContainer}>
-      <h1>Contactez-moi</h1>
+      <h1>Contactez-moi :</h1>
+      <form name="contact" method="post" className={styles.form}>
+        <label for="name">Nom : </label>
+        <input type="text" name="name" placeholder="Nom" />
+
+        <label for="email">Email : </label>
+        <input type="email" name="email" placeholder="Email" />
+
+        <label for="message">Message : </label>
+        <textarea name="message" placeholder="Message"></textarea>
+
+        <button type="submit">Envoyer</button>
+        {/*<Button />*/}
+      </form>
+      ,
     </div>
   );
 }
