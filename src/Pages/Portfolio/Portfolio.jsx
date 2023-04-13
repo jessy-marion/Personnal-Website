eae2314bb94afefa5b79373ccfb7c708a513aad9
Jessy Marion <jessymarionmail@gmail.com>
2023-04-13 17:45:24 +0200

    feat: portfolio part II

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -4,7 +4,7 @@ import p1Img from "../../assets/img/p1.PNG";
 import p2Img from "../../assets/img/p2.PNG";
 import p3Img from "../../assets/img/p3.PNG";
 import p4Img from "../../assets/img/p4.PNG";
-import p5Img from "../../assets/img/p5.png";
+import p5Img from "../../assets/img/p5.PNG";
 import p6Img from "../../assets/img/p6.PNG";
 import p7Img from "../../assets/img/R quality.png";
 
