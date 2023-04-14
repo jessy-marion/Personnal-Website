98bafc2684c37d18a88f50ea22fe3c9c620a9aac
Jessy Marion <jessymarionmail@gmail.com>
2023-04-14 11:29:02 +0200

    feat: favicon & titleÃ

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,14 +1,10 @@
 import styles from "./Card.module.scss";
-function Card({ props, image }) {
+function Card({ children, image, h2 }) {
   return (
     <div className={styles.card}>
       <img src={image} alt="" />
-      <h2>test</h2>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores
-        aspernatur, consequatur deleniti dolorem dolorum fuga odit officiis
-        soluta tempora unde.
-      </p>
+      <h2>{h2}</h2>
+      <p>{children}</p>
     </div>
   );
 }
--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -13,13 +13,41 @@ function Portfolio() {
     <main className={styles.portfolio}>
       <h1>Portfolio</h1>
       <div className={styles.cardContainer}>
-        <Card image={p7Img} />
-        <Card image={p6Img} />
-        <Card image={p5Img} />
-        <Card image={p4Img} />
-        <Card image={p3Img} />
-        <Card image={p2Img} />
-        <Card image={p1Img} />
+        <Card h2={"Air quality Project"} image={p7Img}>
+          (Coming soon...) Dernier projet en date : Une application web qui
+          donne l'indice de qualité de l'air, en direct, d'un lieu choisi.
+          Réalisé avec React <i className="fa-brands fa-react"></i>.
+        </Card>
+        <Card h2={"Kasa"} image={p6Img}>
+          Création, à partir d'une maquette Figma, de la partie front-end d'un
+          site d'hébergement. Réalisé avec React{" "}
+          <i className="fa-brands fa-react"></i>.
+        </Card>
+        <Card h2={"Piiquante / Hot Takes"} image={p5Img}>
+          Création d'une API sécurisée pour une application d'avis
+          gastronomique. Utilisation d'une base de donnée noSQL. Réalisé avec
+          Node.js, Express.js et mongoDB{" "}
+          <i className="fa-brands fa-node-js"></i>.
+        </Card>
+        <Card h2={"Kanap"} image={p4Img}>
+          Conception d'un site de E-commerce qui a pour articles des canapés.
+          Réalisé avec du Javascript vanilla{" "}
+          <i className="fa-brands fa-square-js"></i>
+        </Card>
+        <Card h2={"La panthère"} image={p3Img}>
+          Optimisation SEO du site d'une agence web. Amélioration de l'UX.
+        </Card>
+        <Card h2={"Ohmyfood"} image={p2Img}>
+          Réalisation, à partir d'une maquette, de la partie intégration d'un
+          site de réservation de menus pour divers restaurants. Réalisé avec
+          HTML et SASS. <i className="fa-brands fa-html5"></i>{" "}
+          <i className="fa-brands fa-sass"></i>
+        </Card>
+        <Card h2={"Booki"} image={p1Img}>
+          Intégration d'un site d'hébergement dans l'esprit de "airbnb". Réalisé
+          en HTML et CSS <i className="fa-brands fa-html5"></i>{" "}
+          <i className="fa-brands fa-css3-alt"></i>
+        </Card>
       </div>
     </main>
   );
