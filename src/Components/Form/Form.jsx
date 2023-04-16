def9616e61c53d3eb78a19f13166046bf34c3f86
Jessy Marion <jessymarionmail@gmail.com>
2023-04-16 10:58:59 +0200

    fix: form

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -4,7 +4,14 @@ function Form() {
   return (
     <div className={styles.formContainer}>
       <h1>Contactez-moi :</h1>
-      <form name="contact" method="post" className={styles.form} netlify>
+      <form
+        name="contact"
+        method="post"
+        className={styles.form}
+        netlify
+        onSubmit="submit"
+      >
+        <input type="hidden" name="form-name" value="contact" />
         <label htmlFor="name">Nom : </label>
         <input type="text" name="name" placeholder="Nom" />
 
