1325401c5d7f7b854a2734d055593bd56db3907e
jessy <jessymarionmail@gmail.com>
2024-07-16 17:11:25 +0200

    fix CV

--- src/Pages/CV/CV.jsx
+++ src/Pages/CV/CV.jsx
@@ -6,9 +6,12 @@ function CV() {
   return (
     <main className={styles.main}>
       <h1 className={`${styles.title} {/*b1*/}`}>Curriculum vitae</h1>
-      <h2>"Voici mon CV au format PDF"</h2>
+      <section>
+      <h2>"Voici mon CV au format PDF" :</h2>
       <br />
       <Button pdf={pdf}>Télécharger le CV</Button>
+      </section>
+      
     </main>
   );
 }
